7ea7ad936771e291660c3c2bcc4689339f57e19e
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-07-20 22:53:49 +0000

    remove unused

--- mobile_app/lib/auth/auth_gate.dart
+++ mobile_app/lib/auth/auth_gate.dart
@@ -4,7 +4,7 @@ import 'package:farmnets/screens/farmer/farmer_auth_screen.dart';
 import 'package:farmnets/screens/farmer/farmer_home_screen.dart';
 import 'package:farmnets/screens/ext_officer/officer_home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
+//import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:farmnets/providers/user_provider.dart';
